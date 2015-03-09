e2841c3da99bcc10ce1c23dbbdd2f4a0f0bf857e
Brian Chojnowski <chow@vajri.com>
2015-03-09 18:00:51 -0500

    Udacity P3 project submission

--- js/app.js
+++ js/app.js
@@ -1,11 +1,33 @@
+// Game Parameters
+var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+};
+
+var blockWidth = 101;
+var blockHeight = 83;
+var enemyStartY = [60, 60 + blockHeight, 60 + blockHeight * 2];
+var enemySpeed = [100, 130, 200, 250, 300];
+var playerStartX = 202;
+var playerStartY = 392;
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.initialize();
+}
 
+Enemy.prototype.initialize = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
+    this.x = -blockWidth;
+    this.y = enemyStartY[Math.floor(Math.random() * 3)];
+    this.speed = enemySpeed[Math.floor(Math.random() * 5)];
 }
 
 // Update the enemy's position, required method for game
@@ -14,6 +36,15 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
+
+    if (this.x > blockWidth * 9) {
+        this.initialize();
+    }
+
+    if (this.y === player.y && (Math.abs(this.x - player.x) < (blockWidth / 2))) {
+        player.initialize();
+    }
 }
 
 // Draw the enemy on the screen, required method for game
@@ -25,22 +56,112 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function() {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.initialize();
+
+    switch(Math.floor(Math.random() * 5)) {
+        case 0 :
+            this.sprite = 'images/char-boy.png';
+        break;
+
+        case 1 :
+            this.sprite = 'images/char-cat-girl.png';
+        break;
+
+        case 2 :
+            this.sprite = 'images/char-horn-girl.png';
+        break;
+
+        case 3 :
+            this.sprite = 'images/char-pink-girl.png';
+        break;
+
+        case 4 :
+            this.sprite = 'images/char-princess-girl.png';
+        break;
+
+        default:
+            this.sprite = 'images/char-boy.png';
+    }
+}
+
+Player.prototype.initialize = function() {
+    this.x = playerStartX;
+    this.y = playerStartY;
+    this.move = null;
+}
+
+// Update the player's position, required method for game
+// Parameter: dt, a time delta between ticks
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    switch(this.move) {
+        case 'left' :
+            if (this.x > 0) {
+                this.x -= blockWidth;
+            }
+        break;
+
+        case 'up' :
+            this.y -= blockHeight;
+        break;
+        
+        case 'right' :
+            if (this.x < blockWidth * 5) {
+                this.x += blockWidth;
+            }
+        break;
+        
+        case 'down' :
+            if (this.y < playerStartY) {
+                this.y += blockHeight;
+            }
+        break;
+
+        default:
+    }
+    this.move = null;
+
+    if (this.y < 60) {
+        this.initialize();
+        this.lives -= 1;
+    }
+}
+
+// Draw the player on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+// Draw the player on the screen, required method for game
+Player.prototype.handleInput = function(direction) {
+    this.move = direction;
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [new Enemy(), new Enemy(), new Enemy(), new Enemy()];
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+// disable default key actions
+document.addEventListener('keydown', function(e) {
+    if (typeof allowedKeys[e.keyCode] === 'undefined') {
+        e.preventDefault();
+    }
+}, false);
+
